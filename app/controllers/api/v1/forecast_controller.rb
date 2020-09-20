1723130f7dad3ac72e91b83f79554ae627e1be22
Gaby Mendez <gmendez90@gmail.com>
2020-09-20 18:14:22 -0400

    Create directories to match namespacing for forecast_controller

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::ForecastController < ApplicationController
+  def show
+    city_state = location_params[:location].split(',')
+    location = RESULTS.location(city_state)
+    forecast = RESULTS.get_forecast(location.coordinates)
+    render json: ForecastSerializer.new(forecast)
+  end
+
+  private
+
+  RESULTS ||= ResultsFacade.new
+
+  def location_params
+    params.permit(:location)
+  end
+end
