acf40d8bc3ba06c88c7616be4b3cf66263d53155
Gaby Mendez <gmendez90@gmail.com>
2020-09-20 17:53:39 -0400

    Edit location_spec

--- spec/poros/location_spec.rb
+++ spec/poros/location_spec.rb
@@ -1,19 +1,22 @@
 RSpec.describe Location do
+
   before :each do
-    @denver_lat = '39.738453'
-    @denver_long = '-104.984853'
-    city = 'denver'
-    state = 'co'
-    @location = Location.new(city, state)
+    details = {
+      city: 'Denver',
+      state: 'CO',
+      coordinates: {
+        lat: '39.738453',
+        lng: '-104.984853'
+      }
+    }
+
+    @location = Location.new(details)
   end
 
-  xit 'has attributes: city, state' do
+  it 'has attributes: city, state, coordinates' do
     expect(@location.city).to eq('Denver')
     expect(@location.state).to eq('CO')
-  end
-
-  xit '#coordinates' do
-    expect(@location.coordinates[:lat]).to eq(@denver_lat)
-    expect(@location.coordinates[:lng]).to eq(@denver_long)
+    expect(@location.coordinates[:lat]).to eq('39.738453')
+    expect(@location.coordinates[:lng]).to eq('-104.984853')
   end
 end
