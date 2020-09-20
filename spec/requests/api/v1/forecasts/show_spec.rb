f4353f5876469a5490d2d62ec34203a9b921d82b
Gaby Mendez <gmendez90@gmail.com>
2020-09-20 16:25:04 -0400

    Write initial specs for forecast#show and routes

---
+++ spec/requests/api/v1/forecasts/show_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe 'Can get forecast for given city, state in json', type: :request do
+  it 'GET /api/v1/forecast?location=<city>,<state>' do
+
+    location = 'denver,co'
+    headers = { "ACCEPT" => "application/json",
+                "Content-Type" => "application/json" }
+
+    get "/api/v1/forecast", params: { location: location }, headers: headers
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.content_type).to eq("application/json")
+    expect(response).to have_http_status(:success)
+    expect(json[:data][:type]).to eq('forecast')
+    expect(json[:data][:attributes][:city]).to eq('denver')
+    expect(json[:data][:attributes][:state]).to eq('co')
+  end
+end
