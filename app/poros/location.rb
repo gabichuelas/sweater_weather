c16322ff61dcfad4836c75475ec8b7063f305a86
Gaby Mendez <gmendez90@gmail.com>
2020-09-20 17:22:23 -0400

    Write Location PORO class

---
+++ app/poros/location.rb
@@ -0,0 +1,21 @@
+class Location
+  attr_reader :city, :state
+  def initialize(location_params)
+    location_array = location_params[:location].split(',')
+    @city = location_array[0]
+    @state = location_array[1]
+    @results ||= ResultsFacade.new
+  end
+
+  def coordinates
+    @results.get_coordinates(@city, @state)
+    # returns hash { lat: '', long: ''}
+  end
+
+  def formatted_location
+    city = @results.get_city
+    state = @results.get_state
+    # returns official location from geocoding api result
+    { city: city, state: state }
+  end
+end
