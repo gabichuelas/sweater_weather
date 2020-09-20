2ebd0d3447c71c6fe81c3abe1436d61021cac8e7
Gaby Mendez <gmendez90@gmail.com>
2020-09-20 18:21:11 -0400

    Fill out OpenWeatherService#one_call

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,2 +1,22 @@
 class OpenWeatherService
+
+  def one_call(coordinates, exclude)
+    # exclude is a string with comma separated values. Use 'minutely'
+    response = conn.get("onecall?#{coordinates[:lat]}&lon=#{coordinates[:lng]}&exclude=#{exclude}")
+    get_json(response)
+  end
+
+  private
+
+  def get_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.openweathermap.org/data/2.5/") do |req|
+      req.params["appid"] = ENV["OPEN_WEATHER_API_KEY"]
+      req.headers["Accept"] = "application/json"
+      req.headers["Content-Type"] = "application/json"
+    end
+  end
 end
