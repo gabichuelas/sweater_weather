c21d5a99dfab52e4597b162f72c4c4884ec106d9
Gaby Mendez <gmendez90@gmail.com>
2020-09-20 17:42:36 -0400

    Refactor Location PORO

--- app/facades/results_facade.rb
+++ app/facades/results_facade.rb
@@ -3,4 +3,9 @@ class ResultsFacade
     @geocoding ||= GeocodingService.new
     @open_weather ||= OpenWeatherService.new
   end
+
+  def location_details(city, state)
+    @geocoding.location_search(city, state)
+  end
+
 end
--- app/poros/location.rb
+++ app/poros/location.rb
@@ -1,21 +1,11 @@
 class Location
-  attr_reader :city, :state
-  def initialize(location_params)
-    location_array = location_params[:location].split(',')
-    @city = location_array[0]
-    @state = location_array[1]
-    @results ||= ResultsFacade.new
-  end
-
-  def coordinates
-    @results.get_coordinates(@city, @state)
-    # returns hash { lat: '', lng: ''}
-  end
-
-  def formatted_location
-    city = @results.get_city
-    state = @results.get_state
-    # returns official location from geocoding api result
-    { city: city, state: state }
+  RESULTS ||= ResultsFacade.new
+  attr_reader :city, :state, :coordinates
+  
+  def initialize(city, state)
+    details = RESULTS.location_details(city, state)
+    @city = details[:city]
+    @state = details[:state]
+    @coordinates = details[:coordinates]
   end
 end
