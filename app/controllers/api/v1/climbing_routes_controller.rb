2e1aca44a62e37dc5c4f76a0cf0db6b6e512be91
Gaby Mendez <gmendez90@gmail.com>
2020-09-21 11:44:53 -0400

    Create climbingRoutesController#index, initial structure only

--- app/controllers/api/v1/climbing_routes_controller.rb
+++ app/controllers/api/v1/climbing_routes_controller.rb
@@ -1,3 +1,7 @@
-class ClimbingRoutesController < ApplicationController
-  def index; end 
+class Api::V1::ClimbingRoutesController < ApplicationController
+  def index
+    location = RESULTS.location(location_params[:location])
+    # routes = RESULTS.get_routes(location.coordinates)
+    render json: ClimbingRouteSerializer.new(route_options)
+  end
 end
