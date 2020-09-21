09c5a606c4a056d7e4173d91063a6702eec689c7
Gaby Mendez <gmendez90@gmail.com>
2020-09-21 11:38:54 -0400

    Fill out index_spec

--- spec/requests/api/v1/climbing_routes/index_spec.rb
+++ spec/requests/api/v1/climbing_routes/index_spec.rb
@@ -10,5 +10,17 @@ RSpec.describe 'Can get climbing routes for given location', type: :request do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.content_type).to include("application/json")
+
+    expect(json[:data][:type]).to eq("climbing route")
+    expect(json[:data][:attributes]).to include(:location)
+    expect(json[:data][:attributes]).to include(:forecast)
+    expect(json[:data][:attributes]).to include(:routes)
+    expect(json[:data][:attributes][:routes].class).to eq(Array)
+
+    json[:data][:attributes][:routes].each do |route|
+      expect(route).to include(:name)
+      expect(route).to include(:type)
+      expect(route).to include(:distance_to_route)
+    end 
   end
 end
