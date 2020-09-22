b4f207b54ed3d26404bce5face402ec318cc9254
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 18:52:28 -0400

    Remove unecessary attribute from road_trip_serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,8 +1,8 @@
 class RoadTripSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :origin, :destination, :time_in_seconds, :travel_time, :arrival_forecast
+  attributes :origin, :destination, :travel_time, :arrival_forecast
 
   attribute :travel_time do |obj|
     Time.at(obj.time_in_seconds).utc.strftime("%H:%M")
-  end 
+  end
 end
