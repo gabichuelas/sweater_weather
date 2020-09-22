b61727940c93f998938fb20157e1b8dc80d46dca
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 15:03:19 -0400

    Create sessions_controller and route

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(user_params)
+    if user
+      render json: UserSerializer.new(new_user), status: 200
+    else
+      render json: "Bad credentials".to_json, status: 403
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
