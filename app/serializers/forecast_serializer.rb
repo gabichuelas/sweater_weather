08816b53765124e92fbc68ce35a7d32734649227
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 18:29:16 -0400

    Edit forecastSerializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,4 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :timezone, :current, :next_8_days, :next_48_hours
+  attributes :coordinates, :timezone, :current, :next_8_days, :next_48_hours
 end
