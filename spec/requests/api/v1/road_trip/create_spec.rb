5612e90b35e92b4d8188d21efad7ec96bc465d5b
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 18:55:10 -0400

    Add VCR to road_trip # create spec

--- spec/requests/api/v1/road_trip/create_spec.rb
+++ spec/requests/api/v1/road_trip/create_spec.rb
@@ -4,49 +4,54 @@ RSpec.describe 'can return road trip information given start and end destination
   end
 
   it 'POST /api/v1/road_trip' do
+    VCR.use_cassette('trip_denver_pueblo') do
 
-    body = {
-              origin: "Denver,CO",
-              destination: "Pueblo,CO",
-              api_key: @existing_user.api_key
-            }.to_json
+      body = {
+                origin: "Denver,CO",
+                destination: "Pueblo,CO",
+                api_key: @existing_user.api_key
+              }.to_json
 
-    headers = { "ACCEPT" => "application/json",
-                "Content-Type" => "application/json" }
+      headers = { "ACCEPT" => "application/json",
+                  "Content-Type" => "application/json" }
 
-    post api_v1_road_trip_path, params: body, headers: headers
+      post api_v1_road_trip_path, params: body, headers: headers
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      json = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response.content_type).to include("application/json")
-    expect(response.status).to eq(200)
+      expect(response.content_type).to include("application/json")
+      expect(response.status).to eq(200)
 
-    expect(json[:data][:type]).to eq('road_trip')
-    expect(json[:data][:attributes][:origin]).to eq("Denver, CO")
-    expect(json[:data][:attributes][:destination]).to eq("Pueblo, CO")
-    expect(json[:data][:attributes]).to include(:travel_time)
-    expect(json[:data][:attributes]).to include(:arrival_forecast)
-    expect(json[:data][:attributes][:arrival_forecast]).to include(:temp)
-    expect(json[:data][:attributes][:arrival_forecast]).to include(:weather)
-    expect(json[:data][:attributes]).to_not include(:time_in_seconds)
+      expect(json[:data][:type]).to eq('road_trip')
+      expect(json[:data][:attributes][:origin]).to eq("Denver, CO")
+      expect(json[:data][:attributes][:destination]).to eq("Pueblo, CO")
+      expect(json[:data][:attributes]).to include(:travel_time)
+      expect(json[:data][:attributes]).to include(:arrival_forecast)
+      expect(json[:data][:attributes][:arrival_forecast]).to include(:temp)
+      expect(json[:data][:attributes][:arrival_forecast]).to include(:weather)
+      expect(json[:data][:attributes]).to_not include(:time_in_seconds)
+    end
   end
 
   it 'Sad Path: unauthorized api key on POST /api/v1/road_trip' do
-    body = {
-              origin: "Denver,CO",
-              destination: "Pueblo,CO",
-              api_key: "fakeapikey"
-            }.to_json
+    VCR.use_cassette('trip_denver_pueblo') do
 
-    headers = { "ACCEPT" => "application/json",
-                "Content-Type" => "application/json" }
+      body = {
+                origin: "Denver,CO",
+                destination: "Pueblo,CO",
+                api_key: "fakeapikey"
+              }.to_json
 
-    post api_v1_road_trip_path, params: body, headers: headers
+      headers = { "ACCEPT" => "application/json",
+                  "Content-Type" => "application/json" }
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      post api_v1_road_trip_path, params: body, headers: headers
 
-    expect(response.content_type).to include("application/json")
-    expect(response.status).to eq(401)
-    expect(json).to include("401: Unauthorized or missing API key")
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.content_type).to include("application/json")
+      expect(response.status).to eq(401)
+      expect(json).to include("401: Unauthorized or missing API key")
+    end 
   end
 end
