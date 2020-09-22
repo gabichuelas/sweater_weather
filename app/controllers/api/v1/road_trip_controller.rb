df21a270a741e9d6b9bd74b023b72de013f2a1b5
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 17:40:49 -0400

    Write RoadTripController#create

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
+    trip = RESULTS.get_trip_info(params[:origin], params[:destination])
+    render json: TripSerializer.new(trip)
   end
-  
+
 end
