b743bd5883ba5859cc4d3d91bf2d281fcf1df49c
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 15:24:26 -0400

    Use resources syntax for users in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'backgrounds#show'
-      post '/users', to: 'users#create'
+      resources :users, only: [:create]
       post '/sessions', to: 'sessions#create'
     end
   end
