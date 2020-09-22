cbd0068130567e5149118981ee3b39c28fb625c7
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 18:03:32 -0400

    Refactor 401 path in road trip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key: trip_params[:api_key])
+    render json: "401: Unauthorized or missing API key".to_json, status: 401 and return unless user
     trip = RESULTS.get_trip_info(trip_params[:origin], trip_params[:destination])
     render json: RoadTripSerializer.new(trip) and return unless user.nil?
-    render json: "401: Unauthorized or missing API key".to_json, status: 401
   end
 end
