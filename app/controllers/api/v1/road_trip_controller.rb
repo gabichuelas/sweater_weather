d205865c77b5e7c17eae551ffbc587db0a7db30d
Gaby Mendez <gmendez90@gmail.com>
2020-09-22 18:02:14 -0400

    Add 401 path to road_trip_contnroller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-
+    user = User.find_by(api_key: trip_params[:api_key])
     trip = RESULTS.get_trip_info(trip_params[:origin], trip_params[:destination])
-    render json: RoadTripSerializer.new(trip)
+    render json: RoadTripSerializer.new(trip) and return unless user.nil?
+    render json: "401: Unauthorized or missing API key".to_json, status: 401
   end
 end
